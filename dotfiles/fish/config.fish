041952438f8eefd41ac307f55b83cc556765e0d3
vctorgab <vctorgab@gmail.com>
2026-06-05 13:11:05 -0500

    sync: update dotfiles con configuración actual

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -6,3 +6,17 @@ end
 
 # opencode
 fish_add_path /home/gabriel/.opencode/bin
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/gabriel/.local/bin" $PATH
+
+# yazi wrapper: al cerrar yazi, se queda en la carpeta que estabas
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file=$tmp
+    if set cwd (cat -- $tmp); and test -n "$cwd"; and test "$cwd" != "$PWD"
+        cd -- "$cwd"
+    end
+    rm -f -- $tmp
+end
